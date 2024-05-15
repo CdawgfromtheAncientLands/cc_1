1ad0149dc0018f7e4039e668ad645d53b22e4c19
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-05-15 14:12:17 -0400

    Beginning step 4

--- cc_1.js
+++ cc_1.js
@@ -1,7 +1,7 @@
 // 1. Calculate the Tip
 // Determine the tip amount based on the bill using a ternary operator instead of if/else statements.
 
-// the Bill Threshold is determined by Steven. He will pay a 15% tip for bills inside the threshold and a 20% for others.
+// the Bill Threshold is determined by Steven's culture. He will pay a 15% tip for bills inside the threshold and a 20% for others.
 let initialBill = 275;
 let billLowerThreshold = 50;
 let billUpperThreshold = 300;
@@ -14,21 +14,32 @@ function calcTip(startingBill) {
 
 //Tip amount determined, assigning to variable.
 //Rounding the tip to its nearest two decimal places.
-let roundedTip = (Math.round((calcTip(initialBill) + Number.EPSILON) * 100) / 100);
+//Changed variable name to meet requirements.
+let tip = (Math.round((calcTip(initialBill) + Number.EPSILON) * 100) / 100);
 
 //2. Output Details
 //Display a message in the console that includes the bill amount, the tip amount, and the total cost (bill plus tip).
 
-totalCost = (initialBill + roundedTip);
+totalCost = (initialBill + tip);
 //Ensured that numbers would go out to 2 decimal places. Not necessary for intialBill.
-console.log("The initial bill was $" + initialBill + ", and the tip was $" + roundedTip.toFixed(2) + ". The total cost came out to be $" + totalCost.toFixed(2) + ".");
+console.log("The initial bill was $" + initialBill + ", and the tip was $" + tip.toFixed(2) + ". The total cost came out to be $" + totalCost.toFixed(2) + ".");
 
-//3. Develop a function named calcTip that accepts any bill amount as an input and returns the calculated tip, \
+//3. Develop a function named calcTip that accepts any bill amount as an input and returns the calculated tip, 
 // following the specified rules. 
 // Test this function with a bill value of $100.
 
 //Such a function has been made during my completion of step 1. Name updated to become calcTip.
 //Testing function...
-let testBill = 100
-testTip = calcTip(testBill)
-console.log (testTip)
+let testBill = 100;
+testTip = calcTip(testBill);
+console.log("The test tip for a test bill of $" + testBill + " is $" + testTip + ".");
+
+//4. Utilize Arrays
+//Construct an array "bills" containing the test data values.
+//Generate an array, "tips" where each entry is the result of calling your calcTip function for each corresponding bill value.
+//Assemble an array "totals" that sums each bill with its respective tip.
+
+const bills = [125, 555, 44];
+const tips = bills.map(calcTip);
+console.log(bills);
+console.log(tips)
